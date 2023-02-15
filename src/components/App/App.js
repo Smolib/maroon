50f0b11703e0ed870f55847d20ba2cccb10037b2
Mariya Kulakovskaya <kulakovskaya-mariya@yandex.ru>
2023-02-15 13:31:35 +0300

    feat: сделать базовую разметку

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from '../Header/Header.js'
+import Main from "../Main/Main.js";
+import Footer from "../Footer/Footer.js";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import "./Main.css";
+
+function Main() {
+  return (
+    <main>
+     Основное
+    </main>
+  );
+}
+
+export default Main;
