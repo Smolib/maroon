d09ee7f7de42ca117e0127186c0c0d6b3e1669cd
Mariya Kulakovskaya <kulakovskaya-mariya@yandex.ru>
2023-05-20 20:33:44 +0300

    feat: добавить кнопки соцсетей, добавить анимацию

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import logoSvg from "../../images/logo.svg";
 import profileSvg from "../../images/profile.svg";
 import cartSvg from "../../images/cart.svg";
+import facebook from "../../images/facebook.svg";
+import instagram from "../../images/instagram.svg";
+import twitter from "../../images/twitter.svg";
 import burger from "../../images/burger.svg";
 import close from "../../images/close.svg";
 
@@ -38,35 +41,54 @@ function Header() {
       <button className="header__burger-button" onClick={handleBurgerClick}>
         <img alt="Кнопка меню" src={isMenuOpened ? close : burger} />
       </button>
-      <nav
+      <div
         className={`header__nav-area ${
           isMenuOpened ? "header__nav-area_open" : ""
         }`}
       >
-        <ul className="header__nav-list">
-          <li className="header__nav-item">
-            <a className="header__nav-link" href="#">
-              Каталог
+        <nav>
+          <ul className="header__nav-list">
+            <li className="header__nav-item">
+              <a className="header__nav-link" href="#">
+                Каталог
+              </a>
+            </li>
+            <li className="header__nav-item">
+              <a className="header__nav-link" href="#">
+                О нас
+              </a>
+            </li>
+            <li className="header__nav-item">
+              <a className="header__nav-link" href="#">
+                Контакты
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <ul className="header__contacts-list">
+          <li className="header__contact-item">
+            <a className="header__contact-link" href="#">
+              <img className="header__contact-picture" src={facebook} />
             </a>
           </li>
-          <li className="header__nav-item">
-            <a className="header__nav-link" href="#">
-              О нас
+          <li className="header__contact-item">
+            <a className="header__contact-link" href="#">
+              <img className="header__contact-picture" src={instagram} />
             </a>
           </li>
-          <li className="header__nav-item">
-            <a className="header__nav-link" href="#">
-              Контакты
+          <li className="header__contact-item">
+            <a className="header__contact-link" href="#">
+              <img className="header__contact-picture" src={twitter} />
             </a>
           </li>
         </ul>
-      </nav>
+      </div>
       <div className="header__button-area">
         <button className="header__button">
-          <img alt="Профиль" src={profileSvg} />
+          <img className="header__button-icon" alt="Профиль" src={profileSvg} />
         </button>
         <button className="header__button">
-          <img alt="Профиль" src={cartSvg} />
+          <img className="header__button-icon" alt="Профиль" src={cartSvg} />
         </button>
       </div>
     </header>
