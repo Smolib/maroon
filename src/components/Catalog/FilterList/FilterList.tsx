96c0aef57c5906257a48934d6a036403071a3748
Mariya Kulakovskaya <kulakovskaya-mariya@yandex.ru>
2023-06-28 23:07:14 +0300

    feat: добавить адаптивность для фильтра

--- src/components/Catalog/FilterList/FilterList.tsx
+++ src/components/Catalog/FilterList/FilterList.tsx
@@ -1,4 +1,5 @@
 import "./FilterList.scss";
+import { useState } from "react";
 import { TypeOfFilter } from "../../../types/filter";
 import FilterItem from "../FilterItem/FilterItem";
 
@@ -8,13 +9,25 @@ type FilterListProps = {
 
 function FilterList({ item }: FilterListProps) {
   const { title, useFor, keys } = item;
+  const [isListOpen, setIsListOpen] = useState(false);
+  const handleButtonClick = () => {
+    setIsListOpen(!isListOpen);
+  };
   return (
     <fieldset className="filter-list">
-      <div className="filter-list__title-area">
+      <div className="filter-list__title-area" onClick={handleButtonClick}>
         <h3 className="filter-list__title">{title}</h3>
-        <button className="filter-list__button">btn</button>
+        <button
+          type="button"
+          className={`filter-list__button${!isListOpen ? " filter-list__button_open" : ""}`}
+          onClick={handleButtonClick}
+        />
       </div>
-      <ul className="filter-list__list">
+      <ul
+        className={`filter-list__list ${
+          !isListOpen && "filter-list__list_hide"
+        }`}
+      >
         {keys.map((name) => (
           <FilterItem
             name={name}
